19a81bf3a290811077402085700811330f64abbf
Aldy Rizky Putra <aakendev@gmail.com>
2022-08-21 19:09:17 +0700

    Adding Small Revision

--- contacts-app/src/index.js
+++ contacts-app/src/index.js
@@ -3,7 +3,5 @@ import { createRoot } from "react-dom/client";
 import ContactApp from "./components/ContactApp";
 import "./styles/style.css";
 
-const element = <h1>Hello World</h1>;
-
 const root = createRoot(document.getElementById("root"));
 root.render(<ContactApp />);
