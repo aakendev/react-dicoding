0ed837877041fe640e33e7b706be48b2914d8d29
Aldy Rizky Putra <aakendev@gmail.com>
2022-08-22 19:15:54 +0700

    Control Component by Form

--- class-component/src/index.js
+++ class-component/src/index.js
@@ -1,74 +1,94 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-function CounterDisplay({ count }) {
-  if (count === 0) {
-    return <p>{count}</p>;
-  }
-  if (count % 5 === 0 && count % 7 === 0) {
-    return <p>FizzBuzz</p>;
-  }
-  if (count % 5 === 0) {
-    return <p>Fizz</p>;
-  }
-  if (count % 7 === 0) {
-    return <p>Buzz</p>;
-  }
-  return <p>{count}</p>;
-}
-
-function IncreaseButton({ increase }) {
-  return (
-    <div>
-      <button onClick={increase}>+ increase</button>
-    </div>
-  );
-}
-
-function ResetButton({ reset }) {
-  return (
-    <div>
-      <button onClick={reset}>- reset</button>
-    </div>
-  );
-}
-
-class CounterApp extends React.Component {
+class MyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      name: "",
+      email: "",
+      gender: "Man",
     };
-    this.onIncreaseEventHandler = this.onIncreaseEventHandler.bind(this);
-    this.onResetEventHandler = this.onResetEventHandler.bind(this);
+    this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+    this.onEmailChangeEventHandler = this.onEmailChangeEventHandler.bind(this);
+    this.onGenderChangeEventHandler =
+      this.onGenderChangeEventHandler.bind(this);
+    this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
   }
 
-  onIncreaseEventHandler() {
-    this.setState((previousState) => {
-      return {
-        count: previousState.count + 1,
-      };
-    });
-  }
-
-  onResetEventHandler() {
+  onNameChangeEventHandler(event) {
     this.setState(() => {
       return {
-        count: 0,
+        name: event.target.value,
       };
     });
+  }
+
+  onEmailChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        email: event.target.value,
+      };
+    });
+  }
+
+  onGenderChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        gender: event.target.value,
+      };
+    });
+  }
+
+  onSubmitEventHandler(event) {
+    event.preventDefault();
+    const message = `
+      Name: ${this.state.name}
+      Email: ${this.state.email}
+      Gender: ${this.state.gender}
+    `;
+    alert(message);
   }
 
   render() {
     return (
       <div>
-        <IncreaseButton increase={this.onIncreaseEventHandler} />
-        <CounterDisplay count={this.state.count} />
-        <ResetButton reset={this.onResetEventHandler} />
+        <h1>Register Form</h1>
+        <form>
+          <label for="name">Name: </label>
+          <input
+            id="name"
+            type="text"
+            value={this.state.name}
+            onChange={this.onNameChangeEventHandler}
+          />
+          <br></br>
+          <label for="email">Email: </label>
+          <input
+            id="email"
+            type="text"
+            value={this.state.email}
+            onChange={this.onEmailChangeEventHandler}
+          />
+          <br></br>
+          <label for="gender">Gender: </label>
+          <select
+            id="gender"
+            value={this.state.gender}
+            onChange={this.onGenderChangeEventHandler}
+          >
+            <option value="Man">Man</option>
+            <option value="Woman">Woman</option>
+          </select>
+          <br></br>
+          <button type="submit" onClick={this.onSubmitEventHandler}>
+            Submit
+          </button>
+        </form>
       </div>
     );
   }
 }
 
 const root = createRoot(document.getElementById("root"));
-root.render(<CounterApp />);
+root.render(<MyForm />);
